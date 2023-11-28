c681dd5688c1ac1b35c7d7b8a0645b85cc2bef0e
KoshekNj <tea.zupanovic@gmail.com>
2023-11-28 22:59:00 +0100

    added bottoms and new assets in second dress up game

--- DressUp/dressup2.js
+++ DressUp/dressup2.js
@@ -2,27 +2,48 @@ const previousDress = document.getElementById("previous-button__dress");
 const nextDress = document.getElementById("next-button__dress");
 const previousShoes = document.getElementById("previous-button__shoes");
 const nextShoes = document.getElementById("next-button__shoes");
+const previousBottoms = document.getElementById("previous-button__bottoms");
+const nextBottoms = document.getElementById("next-button__bottoms");
 const dress = document.getElementById("doll__dress");
 const shoes = document.getElementById("doll__shoes");
+const bottoms = document.getElementById("doll__bottoms");
 
 let indexDress = 0;
 let indexShoes = 0;
+let indexBottoms = 0;
+
 previousDress.classList.add("hide");
 previousShoes.classList.add("hide");
+previousBottoms.classList.add("hide");
 previousDress.setAttribute("disabled", "");
 previousShoes.setAttribute("disabled", "");
+previousBottoms.setAttribute("disabled", "");
+
 const clothes = [
   {
     dress: "",
+    bottoms: "",
     shoes: "",
   },
   {
-    dress: "./DressPurple1.png",
-    shoes: "./ShoeGothBoots.png",
+    dress: "./Assets2/DressPurple1.png",
+    bottoms: "./Assets2/CrossJeans.png",
+    shoes: "./Assets2/ShoeGothBoots.png",
   },
   {
-    dress: "",
+    dress: "./Assets2/WhiteCorset.png",
+    bottoms: "./Assets2/LeatherBucklePants.png",
     shoes: "",
+  },
+  {
+    dress: "./Assets2/BowTop.png",
+    bottoms: "./Assets2/LowRiseJeans.png",
+    shoes: "./Assets2/PointyHeels.png",
+  },
+  {
+    dress: "./Assets2/PVCCorset.png",
+    bottoms: "./Assets2/WhiteSilkyMidiSkirt.png",
+    shoes: "./Assets2/PointyHeels.png",
   },
 ];
 
@@ -43,8 +64,17 @@ nextShoes.addEventListener("click", () => {
 
 previousShoes.addEventListener("click", () => {
   indexShoes--;
-  console.log(indexShoes);
   setShoes();
+});
+
+nextBottoms.addEventListener("click", () => {
+  indexBottoms++;
+  setBottoms();
+});
+
+previousBottoms.addEventListener("click", () => {
+  indexBottoms--;
+  setBottoms();
 });
 
 function setDress() {
@@ -85,10 +115,33 @@ function setShoes() {
   }
 }
 
+function setBottoms() {
+  showBottoms(clothes[indexBottoms]);
+  if (indexBottoms == 0) {
+    previousBottoms.classList.add("hide");
+    previousBottoms.setAttribute("disabled", "");
+  }
+  if (indexBottoms > 0 && indexBottoms < clothes.length - 1) {
+    previousBottoms.classList.remove("hide");
+    nextBottoms.classList.remove("hide");
+    previousBottoms.removeAttribute("disabled", "");
+    nextBottoms.removeAttribute("disabled", "");
+  }
+
+  if (indexBottoms == clothes.length - 1) {
+    nextBottoms.classList.add("hide");
+    nextBottoms.setAttribute("disabled", "");
+  }
+}
+
 function showDress() {
   dress["src"] = clothes[indexDress].dress;
 }
 
 function showShoes() {
   shoes["src"] = clothes[indexShoes].shoes;
+}
+
+function showBottoms() {
+  bottoms["src"] = clothes[indexBottoms].bottoms;
 }
